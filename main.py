33f4b60ff16d866d2140028b8715856deafd2c1c
Vartabg <garo.vartabed@gmail.com>
2025-04-10 19:01:31 -0500

    Fixed incorrect GUI class import from gui.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python3
-"""
-Dobscrape v4 - DOB Violations Scraper
-Main entry point that launches the GUI
-"""
-
 import sys
 from PyQt6.QtWidgets import QApplication
-from gui import MainWindow
+from gui import DOBScraperGUI
 
 def main():
-    """Launch the DOB Violations Scraper GUI application"""
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = DOBScraperGUI()
     window.show()
     sys.exit(app.exec())
 
